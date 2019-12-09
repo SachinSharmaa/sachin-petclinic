322a6e2120ff55bdc002db7b6c3f20df51037889
Sachin <sachinsharmaa15@gmail.com>
2019-12-10 01:07:29 +0530

    Removing entity annotation from Person

--- pet-clinic-data/src/main/java/com/sachin/petclinic/entities/Person.java
+++ pet-clinic-data/src/main/java/com/sachin/petclinic/entities/Person.java
@@ -1,14 +1,11 @@
 package com.sachin.petclinic.entities;
 
-import javax.persistence.Entity;
-
 import lombok.Data;
 
 /**
  * @author Sachin Sharma
  */
 @Data
-@Entity
 public class Person {
 
 	private String firstName;
